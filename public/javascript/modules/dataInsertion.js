a5715ad38ef658e5f61727645d6f2caf7fe7cdb6
brindidrip <dmarti70@uic.edu>
2019-01-09 17:53:33 -0600

    clean

--- public/javascript/modules/dataInsertion.js
+++ public/javascript/modules/dataInsertion.js
@@ -11,7 +11,7 @@ exports.insertUser = function(db, req, hash, callback) {
          "fullName" : req.body.firstName + " " + req.body.lastName, 
          "country" : req.body.country,
          "birthdate" :  req.body.dobMonth + req.body.dobDay + req.body.dobYear,
-         "creation" : today.getMonth() + '-' + today.getDate() + '-' + today.getFullYear(),
+         "creation" : (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear(),
          "membership" : false
       },
       "username" : req.body.user_reg,
